78d802c2d3d1f22b1fab893e078f2d6032b7fc1f
italogar6 <italogar6@hotmail.com>
2015-10-27 12:06:57 +0000

    Mas celdis

--- src/Practica2/Celda.java
+++ src/Practica2/Celda.java
@@ -13,12 +13,18 @@ package Practica2;
 public class Celda {
     private static int idSeed = 0;
     private final int id;
+    
+    //Tipo de celda
     protected char tipo;
+    
+    //Estado actual, solo se edita con el AplyState para aplicar el siguiente estado
     private boolean tieneCoche;
     
+    //Estado siguiente, se trata con el generarNextEstado, que tiene que implementar cada celda
     protected boolean nextEstadoTieneCoche;
     protected boolean nextEstadoAceptaCoche;
     
+    //Celdas vecinas
     private final Celda prevCelda;
     private final Celda nextCelda;
 
@@ -46,7 +52,11 @@ public class Celda {
     public boolean tieneCoche(){
         return tieneCoche;
     }
-    
+    /**
+     * 1.- Aquí se genera el nuevo estado, debe sobreescribirse siempre.
+     * 2.- También se debe encargar de ejecutar el nextEstadoAceptaCoche de las
+     * siguientes celdas.
+     */
     protected void generarNextEstado(){
         boolean nextCeldaAceptaCoche = nextCelda.nextEstadoAceptaCoche();
         if (tieneCoche){ //si hay coches en la celda
@@ -70,7 +80,7 @@ public class Celda {
             }
         }
     }
-    
+
     public boolean nextEstadoAceptaCoche(){
         generarNextEstado();
         return nextEstadoAceptaCoche;
---
+++ src/Practica2/CeldaPostInterseccion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica2;
+
+/**
+ *
+ * @author Italo
+ */
+public class CeldaPostInterseccion extends Celda{
+
+    public CeldaPostInterseccion(Celda prevCelda, Celda nextCelda) {
+        super(prevCelda, nextCelda);
+    }
+    
+}
